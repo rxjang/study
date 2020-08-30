c88f2f5a232a3b320d094bfc360812086e72e3a5
rxjang <rxforp@naver.com>
2020-08-30 11:58:07 +0900

    Lambda 3

---
+++ chap14/src/sec05/exam08_and_or_negate_isequal/PredicateIsEqualExample.java
@@ -0,0 +1,24 @@
+package sec05.exam08_and_or_negate_isequal;
+
+import java.util.function.Predicate;
+
+public class PredicateIsEqualExample {
+
+	public static void main(String[] args) {
+		Predicate<String> predicate;
+		
+		predicate = Predicate.isEqual(null);
+		System.out.println("null,null: "+predicate.test(null));
+		
+		predicate = Predicate.isEqual("java8");
+		System.out.println("null,java8: "+predicate.test(null));
+		
+		predicate = Predicate.isEqual("null");
+		System.out.println("null,java8: "+predicate.test("java8"));
+
+		predicate = Predicate.isEqual("java8");
+		System.out.println("java8,java8: "+predicate.test("java8"));
+
+	}
+
+}
