92671329f9ab7ca91a4d9a9c383ac87498b860aa
장서현 <olllehyun@gmail.com>
2021-08-03 23:15:53 +0900

    basic of array

---
+++ standardOfJava/src/chap05/ArrayEx2.java
@@ -0,0 +1,15 @@
+package chap05;
+
+import java.util.Arrays;
+
+public class ArrayEx2 {
+    public static void main(String[] args) {
+        int [] array1 = new int[]{100, 95, 80, 70, 60};
+        char[] chArr = {'a', 'b', 'c', 'd'};
+
+        System.out.println(Arrays.toString(array1));
+        System.out.println(array1);
+        System.out.println(Arrays.toString(chArr));
+        System.out.println(chArr);
+    }
+}
