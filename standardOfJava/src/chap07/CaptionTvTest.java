cecf8dedcdf1c8cb8fdc6cad2b70a9a729072aea
장서현 <olllehyun@gmail.com>
2021-08-08 18:54:50 +0900

    inheritance

--- standardOfJava/src/chap07/CaptionTvTest.java
+++ standardOfJava/src/chap07/CaptionTvTest.java
@@ -1,2 +1,31 @@
-package chap07;public class CaptionTvTest {
+package chap07;
+
+class Tv {
+    boolean power;
+    int channel;
+
+    void power() { power = !power; }
+    void channelUp() { ++channel; }
+    void channelDown() { --channel; }
+}
+
+class CaptionTv extends Tv{
+    boolean caption;
+    void displayCaption(String text) {
+        if(caption) {
+            System.out.println(text);
+        }
+    }
+}
+
+class CaptionTvTest {
+    public static void main(String[] args) {
+        CaptionTv ctv = new CaptionTv();
+        ctv.channel = 10;
+        ctv.channelUp();
+        System.out.println(ctv.channel);
+        ctv.displayCaption("Hello, world");
+        ctv.caption = true;
+        ctv.displayCaption("Hello, world");
+    }
 }
---
+++ standardOfJava/src/chap07/DrawShape.java
@@ -0,0 +1,73 @@
+package chap07;
+
+public class DrawShape {
+    public static void main(String[] args) {
+        Point[] p = {
+          new Point(100, 100),
+          new Point(140, 50),
+          new Point(200, 100)
+        };
+
+        Triangle t = new Triangle(p);
+        Circle c = new Circle(new Point(150, 150), 50);
+
+        t.draw();
+        c.draw();
+    }
+}
+
+class Shape {
+    String color = "black";
+    void draw() {
+        System.out.printf("[color=%s]%n", color);
+    }
+}
+
+class Point {
+    int x;
+    int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    Point() {
+        this(0,0);
+    }
+
+    String getXY() {
+        return "(" + x + "," + y + ")";
+    }
+}
+
+class Circle extends Shape {
+    Point center;
+    int r;
+
+    Circle() {
+        this(new Point(0,0), 100);
+    }
+
+    public Circle(Point center, int r) {
+        this.center = center;
+        this.r = r;
+    }
+
+    void draw() {
+        System.out.printf("[center=(%d, %d), r=%d, color=%s]%n", center.x, center.y, r, color);
+    }
+}
+
+class Triangle extends Shape {
+    Point[] p = new Point[3];
+
+    public Triangle(Point[] p) {
+        this.p = p;
+    }
+
+    void draw() {
+        System.out.printf("[pl = %s, p2 %s, p3 = %s, color = %s]%n",
+                p[0].getXY(), p[1].getXY(), p[2].getXY(), color);
+    }
+}
