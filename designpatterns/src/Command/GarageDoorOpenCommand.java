75def9e8a3cb1c85dcc7843a3fada36ed6b37e4d
장서현 <olllehyun@gmail.com>
2021-10-24 22:45:17 +0900

    Command pattern 1

---
+++ designpatterns/src/Command/GarageDoorOpenCommand.java
@@ -0,0 +1,15 @@
+package Command;
+
+public class GarageDoorOpenCommand implements Command{
+
+    GarageDoor garageDoor;
+
+    public GarageDoorOpenCommand(GarageDoor garageDoor) {
+        this.garageDoor = garageDoor;
+    }
+
+    @Override
+    public void execute() {
+        garageDoor.up();
+    }
+}
