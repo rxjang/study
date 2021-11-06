87994405cd47b40afcd0f60428ad4b025639371d
장서현 <olllehyun@gmail.com>
2021-11-06 12:46:49 +0900

    Hollywood Principle

---
+++ designpatterns/src/templatemethod/applet/MyApplet.java
@@ -0,0 +1,32 @@
+package templatemethod.applet;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class MyApplet extends Applet {
+    String message;
+
+    public void init() {
+        message = "안녕하세요. 제가 왔어요...";
+        repaint();
+    }
+
+    public void start() {
+        message = "시작 중...";
+        repaint();
+    }
+
+    public void stop() {
+        message = "저를 떠나려 하시는군요...";
+        repaint();
+    }
+
+    public void destroy() {
+
+    }
+
+    public void paint(Graphics g) {
+        g.drawString(message, 5, 15);
+    }
+    
+}
---
+++ designpatterns/src/templatemethod/sort/Duck.java
@@ -0,0 +1,25 @@
+package templatemethod.sort;
+
+public class Duck implements Comparable{
+
+    String name;
+
+    int weight;
+
+    public Duck(String name, int weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+        return name + ", weight:" + weight;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        Duck otherDuck = (Duck) o;
+
+        return Integer.compare(this.weight, otherDuck.weight);
+    }
+}
